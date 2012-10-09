c74c194876e0f6668f32c3d5f64a99f7ee95347a
Josh Earl <joshearl1@hotmail.com>
2012-10-08 20:31:25 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DominionDatabase::Application.config.secret_token = 'b61c985e099722ae4751b6205baa003f3de3af6c189c119924286596ce252653eab78585fa4625d443992595356bad2d216b8c80f2966574d7cfbc3805f5844a'
